78bb0d85c9a5a009b9f85b9616003d90df6cdbbd
ShengchaoZhou <1239686711@qq.com>
2025-05-03 19:57:29 +0800

    simple-bean-container

---
+++ src/test/java/org/springframework/test/ioc/SimpleBeanContainerTest.java
@@ -0,0 +1,26 @@
+package org.springframework.test.ioc;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SimpleBeanContainerTest {
+
+    @Test
+    public void testGetBean() throws Exception {
+        BeanFactory beanFactory = new BeanFactory();
+        beanFactory.registerBean("helloService", new HelloService());
+        HelloService helloService = (HelloService) beanFactory.getBean("helloService");
+        // 断言成功并不会输出信息
+        assertThat(helloService).isNotNull();
+        assertThat(helloService.sayHello()).isEqualTo("hello");
+    }
+
+    class HelloService {
+        public String sayHello() {
+            System.out.println("hello");
+            return "hello";
+        }
+    }
+}
