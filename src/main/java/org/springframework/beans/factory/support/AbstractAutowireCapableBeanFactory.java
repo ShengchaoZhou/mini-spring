ab2b46cfae51c96aed556936eaf39e6143ce6a65
ShengchaoZhou <1239686711@qq.com>
2025-05-05 21:29:17 +0800

    instantiation-strategy

--- src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -8,16 +8,28 @@ import org.springframework.beans.factory.config.BeanDefinition;
  * @date 2025/5/3 21:47
  */
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
+    private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
+    public InstantiationStrategy getInstantiationStrategy() {
+        return instantiationStrategy;
+    }
+
+    public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+        this.instantiationStrategy = instantiationStrategy;
+    }
+
+    protected Object createBeanInstance(BeanDefinition beanDefinition) {
+        return getInstantiationStrategy().instantiate(beanDefinition);
+    }
+
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
         return doCreateBean(beanName, beanDefinition);
     }
 
     protected Object doCreateBean(String beanName, BeanDefinition beanDefinition) {
-        Class beanClass = beanDefinition.getBeanClass();
         Object bean = null;
         try {
-            bean = beanClass.newInstance();
+            bean = createBeanInstance(beanDefinition);
         } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
