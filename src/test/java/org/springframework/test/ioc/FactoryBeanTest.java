12ae603f4247f6453ea8a544b256852303e44b84
ShengchaoZhou <1239686711@qq.com>
2025-05-10 21:40:53 +0800

    factory-bean

---
+++ src/test/java/org/springframework/test/ioc/FactoryBeanTest.java
@@ -0,0 +1,22 @@
+package org.springframework.test.ioc;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.ioc.bean.Car;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * @author Shengchao Zhou
+ * @date 2025/5/10 21:38
+ */
+public class FactoryBeanTest {
+
+    @Test
+    public void testFactoryBean() throws Exception {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:factory-bean.xml");
+
+        Car car = applicationContext.getBean("car", Car.class);
+        assertThat(car.getBrand()).isEqualTo("porsche");
+    }
+}
