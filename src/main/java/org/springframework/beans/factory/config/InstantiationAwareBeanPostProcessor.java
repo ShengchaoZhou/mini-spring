61dcbca1514cea742c0791b7061165cd12797a27
ShengchaoZhou <1239686711@qq.com>
2025-05-15 21:02:48 +0800

    auto-proxy

---
+++ src/main/java/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package org.springframework.beans.factory.config;
+
+import org.springframework.beans.BeansException;
+
+/**
+ * @author Shengchao Zhou
+ * @date 2025/5/15 20:30
+ */
+public interface InstantiationAwareBeanPostProcessor extends BeanPostProcessor {
+
+    /**
+     * 在bean实例化之前执行
+     *
+     * @param beanClass
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException;
+}
