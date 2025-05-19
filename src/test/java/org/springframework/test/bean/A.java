e9426bd02f1413d251e1ec1966938e1295061d19
ShengchaoZhou <1239686711@qq.com>
2025-05-19 17:23:34 +0800

    circular-reference-without-proxy-bean

---
+++ src/test/java/org/springframework/test/bean/A.java
@@ -0,0 +1,20 @@
+package org.springframework.test.bean;
+
+/**
+ * @author Shengchao Zhou
+ * @date 2025/5/19 17:02
+ */
+public class A {
+
+    private B b;
+
+    public void func(){}
+
+    public B getB() {
+        return b;
+    }
+
+    public void setB(B b) {
+        this.b = b;
+    }
+}
---
+++ src/test/java/org/springframework/test/bean/B.java
@@ -0,0 +1,18 @@
+package org.springframework.test.bean;
+
+/**
+ * @author Shengchao Zhou
+ * @date 2025/5/19 17:03
+ */
+public class B {
+
+    private A a;
+
+    public A getA() {
+        return a;
+    }
+
+    public void setA(A a) {
+        this.a = a;
+    }
+}
